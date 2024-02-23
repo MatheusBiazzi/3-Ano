00c1d3e2b45568f7eefbecc4d6f12eda593b348d
MatheusBiazzi <124881548+MatheusBiazzi@users.noreply.github.com>
2024-02-23 09:34:38 -0300

    Add files via upload

---
+++ prog_back_end/aula23_02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $bla = "<h1>Um sonho?</h1>";
+        echo $bla;
+        echo "<p>Primeira divisão... \u{1F602}</p>";
+
+        $zctba ='imgas/coxa.jpg';
+       
+    ?>
+    <img src="<?php echo $zctba;?>" alt="coxa">
+</body>
+</html>
+
+
